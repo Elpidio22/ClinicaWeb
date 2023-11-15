70acd105854efa7f8a68f8b48b63648337a4d387
Elpidio Luna <elpidioluna6@gmail.com>
2023-11-15 00:08:20 -0300

    Agregando Frontend

---
+++ ClinicaWebFront/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+export const Sidebar = () => {
+return (
+<>
+    <aside className="sidebar">
+      <ul>
+        <li>
+          <a href="#">Pacientes</a>
+        </li>
+        <li>
+          <a href="#">Médicos</a>
+        </li>
+        <li>
+          <a href="#">Especialidad</a>
+        </li>
+        <li>
+          <a href="#">Turnos</a>
+        </li>
+      </ul>
+    </aside>
+</>
+);
+};
